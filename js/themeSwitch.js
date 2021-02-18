8266d7fd8e595ed7736a88319bcea85f3949f247
Vladislaw <vladik20993@gmail.com>
2021-02-18 13:49:20 +0300

    Работа с localStorage
    Первый шаг на пути к сохранению браузером данных выбора темы

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,13 +1,17 @@
+
 let switchMode = document.getElementById("switchMode");
 
 switchMode.onclick = function() {
     let theme = document.getElementById("theme");
+    localStorage.setItem('theme', theme.href); //теперь у вас в localStorage хранится ключ "myKey" cо значением "myValue"
 
-    if (theme.getAttribute("href") == "./css/light-mode.css") {
-        theme.href = "./css/dark-mode.css";
+
+    if (localStorage.theme.getAttribute("href") == "./css/light-mode.css") {
+        localStorage.theme.href = "./css/dark-mode.css";
         document.getElementById("switchMode").innerHTML = "Перейти в светлую тему";
+        //localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
     } else {
-        theme.href = "./css/light-mode.css";
+        localStorage.theme.href = "./css/light-mode.css";
         document.getElementById("switchMode").innerHTML = "Перейти в тёмную тему";
     }
 }
@@ -17,3 +21,36 @@ swMode.onclick = function() {
     swMode.toggleClass('switch-on');
     console.log("!");
 };
+
+// Работа с localStorage
+//Добавляем или изменяем значение:
+// localStorage.setItem('theme', 'default'); //теперь у вас в localStorage хранится ключ "myKey" cо значением "myValue"
+
+//Выводим его в консоль:
+// var localValue = localStorage.getItem('theme');
+// console.log(localValue);
+
+// localStorage["Ключ"] = "Значение" установка значения
+// localStorage["Ключ"]  Получение значения
+// delete localStorage["Ключ"]  Удаление значения
+/*
+Storage.key(n) — имя ключа с индексом n
+Storage.getItem() — получить элемент
+Storage.setItem() — записать элемент
+Storage.removeItem() — удалить элемент
+Storage.clear() — очистить хранилище
+Storage.length — длина хранилища (количество элементов — пар ключ/значение)
+*/
+
+// //создадим объект
+// var obj = {
+// 	item1: 1,
+// 	item2: [123, "two", 3.0],
+// 	item3:"hello"
+// };
+
+// var serialObj = JSON.stringify(obj); сериализуем его
+
+// localStorage.setItem("myKey", serialObj); запишем его в хранилище по ключу "myKey"
+
+// var returnObj = JSON.parse(localStorage.getItem("myKey")) спарсим его обратно объект
