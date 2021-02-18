05a9c7402490cc68ddb9bc8324bec114cc8ac0d5
Vladislaw <vladik20993@gmail.com>
2021-02-19 01:58:54 +0300

    ver 0.1 +
    Немного украшательств к коду, используя Prettier

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,21 +1,20 @@
 let switchMode = document.getElementById("switchMode");
 
-switchMode.onclick = function() {
-    let theme = document.getElementById("theme");
-    // localStorage.setItem('theme', theme.href); //теперь у вас в localStorage хранится ключ "myKey" cо значением "myValue"
+switchMode.onclick = function () {
+  let theme = document.getElementById("theme");
+  // localStorage.setItem('theme', theme.href); //теперь у вас в localStorage хранится ключ "myKey" cо значением "myValue"
 
+  if (theme.getAttribute("href") == "./css/light-mode.css") {
+    theme.href = "./css/dark-mode.css";
+    document.getElementById("switchMode").innerHTML = "Перейти в светлую тему";
+  } else {
+    theme.href = "./css/light-mode.css";
+    document.getElementById("switchMode").innerHTML = "Перейти в тёмную тему";
+  }
+};
 
-    if (theme.getAttribute("href") == "./css/light-mode.css") {
-        theme.href = "./css/dark-mode.css";
-        document.getElementById("switchMode").innerHTML = "Перейти в светлую тему";
-    } else {
-        theme.href = "./css/light-mode.css";
-        document.getElementById("switchMode").innerHTML = "Перейти в тёмную тему";
-    }
-}
-
-let swMode = document.getElementsByClassName('switch-btn');
-swMode.onclick = function() {
-    swMode.toggleClass('switch-on');
-    console.log("!");
+let swMode = document.getElementsByClassName("switch-btn");
+swMode.onclick = function () {
+  swMode.toggleClass("switch-on");
+  console.log("!");
 };
