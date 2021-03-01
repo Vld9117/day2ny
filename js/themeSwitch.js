7c0068a4058873eb22f8ad31f434d11e03d82a4a
Vladislaw <vladik20993@gmail.com>
2021-03-01 03:31:17 +0300

    Little upgrade
    First steps to the saving user data(choosing theme)
    I need some help to resolve problem with refreshing that kills choosed theme.

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,20 +1,54 @@
 let switchMode = document.getElementById("switchMode");
+ //теперь у вас в localStorage хранится ключ "myKey" cо значением "myValue"
+var localValue = localStorage.setItem("theme", "./css/light-mode.css");
+// switchMode.onclick = function () {
+//   let theme = document.getElementById("theme");
 
-switchMode.onclick = function () {
-  let theme = document.getElementById("theme");
-  // localStorage.setItem('theme', theme.href); //теперь у вас в localStorage хранится ключ "myKey" cо значением "myValue"
+//   if (theme.getAttribute("href") == "./css/light-mode.css") {
+//     theme.href = "./css/dark-mode.css";
+//     document.getElementById("switchMode").innerHTML = "Перейти в светлую тему";
+//   } else {
+//     theme.href = "./css/light-mode.css";
+//     document.getElementById("switchMode").innerHTML = "Перейти в тёмную тему";
+//   }
 
-  if (theme.getAttribute("href") == "./css/light-mode.css") {
-    theme.href = "./css/dark-mode.css";
-    document.getElementById("switchMode").innerHTML = "Перейти в светлую тему";
-  } else {
-    theme.href = "./css/light-mode.css";
-    document.getElementById("switchMode").innerHTML = "Перейти в тёмную тему";
+
+//   console.log(localStorage.getItem("theme"));
+// };
+  //попытка перемен
+  switchMode.onclick = function () {
+    let theme = document.getElementById("theme");
+ 
+
+    if (localStorage.theme == "./css/light-mode.css") {
+      theme.href = "./css/dark-mode.css";
+      document.getElementById("switchMode").innerHTML =
+        "Перейти в светлую тему";
+      localStorage.theme = "./css/dark-mode.css";
+    } else {
+      theme.href = "./css/light-mode.css";
+      document.getElementById("switchMode").innerHTML = "Перейти в тёмную тему";
+      localStorage.theme = "./css/light-mode.css";
+    }
+    console.log(theme.href);
+    console.log(localStorage.getItem("theme"));   
   }
-};
 
-let swMode = document.getElementsByClassName("switch-btn");
-swMode.onclick = function () {
-  swMode.toggleClass("switch-on");
-  console.log("!");
-};
+  // document.getElementById('stat').onclick = function() {
+  //   if(document.getElementById('stat').checked) {
+  //     localStorage.setItem('stat', "true");
+  //   } else {
+  //     localStorage.setItem('stat', "false");
+  //   }
+  // }
+  // if (localStorage.getItem('stat') == "true") {
+  //   document.getElementById("stat").setAttribute('checked','checked');
+  // }
+// var localStorage.setItem("theme", "theme.href");
+// console.log(localStorage.getItem("theme"));
+
+// let swMode = document.getElementsByClassName("switch-btn");
+// swMode.onclick = function () {
+//   swMode.toggleClass("switch-on");
+//   console.log("!");
+// }
